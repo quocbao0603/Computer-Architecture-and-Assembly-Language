5759a17812a9cfb46ab0fd1b1f06895e7609db9c
quocbao0603 <qbao0603@gmail.com>
2020-05-01 15:59:52 +0700

    Upload QInt - operators
    Bảo Bảo upload :v:

---
+++ QInt/QInt.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <math.h>
+#include <stdio.h>
+#include <string>
+#define MAX_NUM 4
+using namespace std;
+
+class QInt {
+private:
+	int data[MAX_NUM];
+public:
+	int Get_bit(int k); // lấy bit thứ k của một số QInt
+	void Turn_on_bit(int k); // bật bit thứ k của một số QInt
+ 	QInt operator & (QInt); // Toán tử AND
+	QInt operator | (QInt); // Toán tử OR
+	QInt operator ^ (QInt); // Toán tử XOR 
+	QInt operator ~ (); // Toán tử NOT (lưu ý "~" not "!")
+
+	QInt operator >> (int); // Dịch bit phải (dịch số học) của một số QInt
+	QInt operator << (int); // Dịch bit trái (dịch số học) của một số QInt
+};
